6f25f0ca6c77ba36d191ad235d2224f8217c8cff
m-blank-a <emagsayo4138@gmail.com>
2025-09-16 10:27:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Constants
+const RECTCOUNT = 26;
+
+document.addEventListener("load", (e) => {
+    var elementsList = [];
+    let main = document.getElementById("main");
+    const middleCount = parseInt(Math.ceil(RECTCOUNT * 0.5));
+    
+    for (var i = 0; i < RECTCOUNT; i++) {
+        let element = document.createElement("div");
+    }
+});
